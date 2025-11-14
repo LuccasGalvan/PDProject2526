573131bafa52bfe9c97c1e75312465fed989d2f5
Octavio Marote <octaviomarote@gmail.com>
2025-11-14 09:14:45 +0000

    teste git

--- PDProj/src/pt/isec/pdG36/proj/client/ClientConnection.java
+++ PDProj/src/pt/isec/pdG36/proj/client/ClientConnection.java
@@ -16,7 +16,7 @@ public class ClientConnection {
         this.dirAddr = dirAddr;
         this.dirUdpPort = dirUdpPort;
     }
-    //teste
+    //testea
     public boolean connectToPrimary() {
         ServerEndpoint ep = requestPrimaryFromDirectory();
         if (ep == null) {
